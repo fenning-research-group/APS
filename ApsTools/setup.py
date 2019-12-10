f6144f8ef0a06f939b9bbbff04b633e5c819d3f5
rishi <rek010@eng.ucsd.edu>
2019-12-09 20:59:03 -0600

    make package installable

---
+++ ApsTools/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='apstools',
+    version='0.1dev',
+    packages=['apstools'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('./apstools/README.md').read(),
+)
