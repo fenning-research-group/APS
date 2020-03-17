fca3a24faeeefcdf99c3660b19e73b2003232d9f
rekumar <rishi42@gmail.com>
2020-03-16 21:24:51 -0700

    fix maxlinesperelement argument, was cutting off at certain energy

--- ApsTools/apstools/xrf.py
+++ ApsTools/apstools/xrf.py
@@ -9,10 +9,11 @@ packageDir = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(packageDir, 'include', 'xrfEmissionLines.json'), 'r') as f:
 	emissionLines = json.load(f)
 
-def AddXRFLines(elements, ax = None, maxlinesperelement = 7, tickloc = 'bottom', tickstagger = 0, ticklength = 0.05):        
+def AddXRFLines(elements, ax = None, maxlinesperelement = 4, tickloc = 'bottom', tickstagger = 0, ticklength = 0.05):        
     if ax is None:
     	ax = plt.gca()
     
+    xlim0 = ax.get_xlim()
     stagger = 0
     trans = transforms.blended_transform_factory(ax.transData, ax.transAxes)
     for idx, element in enumerate(elements):
@@ -26,12 +27,15 @@ def AddXRFLines(elements, ax = None, maxlinesperelement = 7, tickloc = 'bottom',
             horizontalalignment = 'right',
             verticalalignment = 'bottom')
 
+        plotted = 0
         for line in emissionLines[element]['xrfEmissionLines']:
-            if (line <= maxlinesperelement) and (line >= 1):
+            if (line <= xlim0[1]) and (line >= xlim0[0]):
 #                 plt.plot([line, line], [0.98 - (idx+1)*ticklength, 0.98 - idx*ticklength], transform = trans, color = color, linewidth = 1.5)
                 if tickloc == 'bottom':
                 	plt.plot([line, line], [0.01 + stagger, 0.01 + ticklength + stagger], transform = trans, color = color, linewidth = 1.5)
                 else:
                 	plt.plot([line, line], [0.99 - stagger, 0.99 - ticklength - stagger], transform = trans, color = color, linewidth = 1.5)
-
+            plotted += 1
+            if plotted >= maxlinesperelement:
+                break
         stagger += tickstagger
