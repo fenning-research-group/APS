7116c6bb37aec0f445d24065a3d3628eafa47f40
rishi <rek010@eng.ucsd.edu>
2021-01-31 08:33:56 -0800

    fix frgtools dependency

--- ApsTools/setup.py
+++ ApsTools/setup.py
@@ -16,7 +16,7 @@ setup(
 		'scikit-image',
 		'cmocean',
 		'pandas',
-		'frgtools @ git+ssh://git@github.com/fenning-research-group/Python-Utilities.git#egg=frgtools&subdirectory=FrgTools',
+		'frgtools @ git+https://github.com/fenning-research-group/Python-Utilities.git#egg=frgtools&subdirectory=FrgTools',
 		],
 	license='Creative Commons Attribution-Noncommercial-Share Alike license',
 	long_description=open('./apstools/README.md').read(),
