a5e7aa71ca905af973ee5eafef82dcc12a101ba5
Rishi E Kumar <47284667+rekumar@users.noreply.github.com>
2022-01-21 12:01:24 -0800

    pip install now includes scattering factors

--- ApsTools/setup.py
+++ ApsTools/setup.py
@@ -1,23 +1,29 @@
 from distutils.core import setup
 
 setup(
-	name='apstools',
-	version='0.2dev',
-	packages=['apstools'],
-	package_dir={'apstools': 'apstools'},
-	package_data={'apstools': ['include/*']},
-	install_requires = [
-		'h5py',
-		'scipy',
-		'matplotlib',
-		'numpy',
-		'opencv-python',
-		'tqdm',
-		'scikit-image',
-		'cmocean',
-		'pandas',
-		'frgtools @ git+https://github.com/fenning-research-group/Python-Utilities.git#egg=frgtools&subdirectory=FrgTools',
-		],
-	license='Creative Commons Attribution-Noncommercial-Share Alike license',
-	long_description=open('./apstools/README.md').read(),
+    name="apstools",
+    version="0.2dev",
+    packages=["apstools"],
+    package_dir={"apstools": "apstools"},
+    package_data={
+        "apstools": [
+            "include/*",
+            "include/scatfacts/*.nff",
+            "include/XAS data files/*.dat",
+        ]
+    },
+    install_requires=[
+        "h5py",
+        "scipy",
+        "matplotlib",
+        "numpy",
+        "opencv-python",
+        "tqdm",
+        "scikit-image",
+        "cmocean",
+        "pandas",
+        "frgtools @ git+https://github.com/fenning-research-group/Python-Utilities.git#egg=frgtools&subdirectory=FrgTools",
+    ],
+    license="Creative Commons Attribution-Noncommercial-Share Alike license",
+    long_description=open("./apstools/README.md").read(),
 )
